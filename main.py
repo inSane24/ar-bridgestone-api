48c3b81ac52dae961edea393aae6ff541561da67
daichisaito-cs <daichi-s@keio.jp>
2025-12-18 16:50:57 +0900

    fix: network

--- main.py
+++ main.py
@@ -105,4 +105,4 @@ def sample(image_path: Path = Path("sample.png")) -> None:
 if __name__ == "__main__":
     host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("main:app", host=host, port=port)
+    uvicorn.run("main:app", host=host, port=port, reload=True)
